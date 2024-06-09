97745851eb94a095dddd5d44e1090aec0d587ad9
Khoa Le <kl58277@uga.edu>
2024-06-09 20:54:25 +0700

    added header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,20 @@
 export default function Header() {
     return (
-        <div>This is the header</div>
+        <div className="flex justify-between">
+            <div>
+                <h1>Khoa Le</h1>
+            </div>
+            <div className="flex justify-between"> 
+                <div>
+                    <h1>About</h1>
+                </div>
+                <div>
+                    <h1>Projects</h1>
+                </div>
+                <div>
+                    <h1>Contact</h1>
+                </div>
+            </div>  
+        </div>
     )
 }
--- components/MainBody.tsx
+++ components/MainBody.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image"
+import profilePic from "../media/profile_pic.jpeg"
+
 export default function MainBody() {
     return (
-        <div className="">Hello World, this is Khoa</div>
+        <div>
+            <div className="">
+                <h1 className="text-xl">Hello World, this is Khoa</h1>
+            </div>
+            <div className="rounded justify-center flex h-72">
+                <div className="flex justify-center border bg-black w-1/5 rounded-lg">
+                    <Image src={profilePic} width={250} height={250} alt="profile_pic" className="self-center rounded-lg"/>
+                </div>
+            </div>
+        </div>
+        
     )
 }
